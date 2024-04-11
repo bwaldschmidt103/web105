26f0492da16b9694d774a3b7da2d852dccfa0f35
bwaldschmidt103 <159931728+bwaldschmidt103@users.noreply.github.com>
2024-04-11 15:14:30 -0500

    Add files via upload

---
+++ grid/change.js
@@ -0,0 +1,19 @@
+function change01() {
+    document.getElementById("large").src = "images/amber.jpg";
+}
+
+function change02() {
+    document.getElementById("large").src = "images/bandit.jpg";
+}
+
+function change03() {
+    document.getElementById("large").src = "images/gizmo.JPG";
+}
+
+function change04() {
+    document.getElementById("large").src = "images/princess-oscar-daisy.jpg";
+}
+
+function change05() {
+    document.getElementById("large").src = "images/princess.JPG";
+}
